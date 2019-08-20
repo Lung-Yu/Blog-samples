617a178c170dbecb7438065b27a0e5253974d019
tygr <workfile975@gmail.com>
2019-08-20 22:07:08 +0800

    mail service.

---
+++ dotNet/form/send_email/send_email/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace send_email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mailTopic = "";
+            string mailContent = "";
+            List<string> targets = new List<string>();
+            targets.Add("example@email.com");
+
+            MailService mailService = MailService.getInstance();
+            mailService.sendNoticeMail(mailTopic, targets.ToArray(), null, mailContent);
+        }
+
+    }
+}
