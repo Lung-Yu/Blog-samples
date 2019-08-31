183c923fb8ab9c2ee2fc990ffe80208f920782f5
tygr <workfile975@gmail.com>
2019-08-31 20:44:39 +0800

    add sample2.

--- code/c and c++/vc++/Win Sample/Project1/Project1/Source.cpp
+++ code/c and c++/vc++/Win Sample/Project1/Project1/Source.cpp
@@ -15,3 +15,11 @@ int WINAPI WinMain(
 	}
 	return 0;
 }
+
+//#include <windows.h>
+//
+//int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+//{
+//    return MessageBox(NULL, "hello, world", "caption", 0);
+//}
+ 
