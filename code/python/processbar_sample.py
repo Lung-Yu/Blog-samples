d5045e706bcc10e78c1c4f08e4281d574cbf0e3c
Tygr <workfile975@gmail.com>
2020-05-24 09:48:27 +0800

    add update method.

--- code/python/processbar_sample.py
+++ code/python/processbar_sample.py
@@ -8,5 +8,23 @@ _widgets = ['[',progressbar.Timer(),']\t',
 
 bar = progressbar.ProgressBar(widgets=_widgets)
 
-for i in bar(range(100)):
-    time.sleep(0.1)
+
+# for i in bar(range(100)):
+#     time.sleep(0.1)
+
+def run_processbar_by_samples(RUN_SAMPLE_COUNT):
+    bar.maxval = RUN_SAMPLE_COUNT
+    bar.start()
+
+    for i in range(RUN_SAMPLE_COUNT):
+        # to do something
+        time.sleep(0.01)
+        bar.update(i+1)
+
+    bar.finish()
+
+def main():
+    run_processbar_by_samples(999)
+
+if __name__ == "__main__":
+    main()
